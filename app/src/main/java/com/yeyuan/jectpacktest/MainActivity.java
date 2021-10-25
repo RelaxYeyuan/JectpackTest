d9789602c8a641c6440ae3f0c5374a8123c2f539
chenhongrui <w94aswc@gmail.com>
2021-10-25 14:04:00 +0800

    Initial commit

---
+++ app/src/main/java/com/yeyuan/jectpacktest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yeyuan.jectpacktest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/yeyuan/jectpacktest/User.java
@@ -0,0 +1,23 @@
+package com.yeyuan.jectpacktest;
+
+
+/**
+ * @author by chenhongrui on 7/3/21
+ *
+ * 内容摘要:
+ * 版权所有：Semisky
+ * 修改内容：
+ * 修改日期
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
